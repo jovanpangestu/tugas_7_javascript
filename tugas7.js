d0e531a9f3554a3b60606aec1a37ccc950c27e07
jovanpangestu <pangestujovan25@gmail.com>
2020-02-06 10:14:31 +0800

    Tugas_7_Javascript
    tugas ini saya kumpulkan untuk NIOMIC

---
+++ tugas7.js
@@ -0,0 +1,11 @@
+function tinggibadan(){
+	var tinggi = [150,155,160,165,170,175,180,185,190,195]
+	for(var x=0; x<tinggi.length; x++){
+		console.log(tinggi[x])
+	console.log("")
+	for(let y of tinggi){
+		console.log(y)
+	}
+	}
+}
+tinggibadan()
